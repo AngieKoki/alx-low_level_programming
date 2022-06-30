ecaae3505505f46a5367042ef0a6bca014b99e71
AngieKoki <angie.dar66@gmail.com>
2022-06-30 14:12:59 -0700

    Hot fix commenting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-/**
- *  * main - prints using the put function
- *   * Return: Always 0 (success)
- *    *
- *     */
 int main(void)
 {
 		char string[] = "\"Programming is like building a multilingual puzzle";
 
-			puts(string);
+		puts(string);
 
-				return (0);
+		return (0);
 }
