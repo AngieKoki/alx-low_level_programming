790be7a2d2966416e29706ea5b43d61def79b239
AngieKoki <angie.dar66@gmail.com>
2022-06-30 14:08:37 -0700

    Writing a C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  * main - prints using the put function
+ *   * Return: Always 0 (success)
+ *    *
+ *     */
+int main(void)
+{
+		char string[] = "\"Programming is like building a multilingual puzzle";
+
+			puts(string);
+
+				return (0);
+}
