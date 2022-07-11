72d05b17a7b6bdc11c2c440e49235bb40c0c8cb8
AngieKoki <angie.dar66@gmail.com>
2022-07-11 13:03:41 -0700

    Remove trailing whitespace

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,18 @@
  * Return: Always 0
  */
 
-void rev_string (char *s)
+void rev_string(char *s)
 {
 	int len = 0;
 	int i;
 
 	while (*(s + len) != '\0')
 		len++;
-	
+
 	for (i = 0; i < len / 2; i++)
 	{
 		char temp = *(s + len - i - 1);
-		*(s + len - i -1) = *(s + i);
+		*(s + len - i - 1) = *(s + i);
 		*(s + i) = temp;
 	}
 }
