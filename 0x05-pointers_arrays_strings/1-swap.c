871411f87f537171c6030cedb4dc788d2361034d
AngieKoki <angie.dar66@gmail.com>
2022-07-11 04:59:09 -0700

    Learning to use temp

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @*a - integer input 1
+ * @*b - integer input 2
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
