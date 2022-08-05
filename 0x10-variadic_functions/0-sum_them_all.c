d946c143420bb11904f003a9f9e951df93f8b21b
AngieKoki <angie.dar66@gmail.com>
2022-08-05 02:37:48 -0700

    Fix betty errors and compilation errors

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,13 +2,13 @@
 
 /**
  * sum_them_all - return sum of all parameters
- * n: input integer
+ * @n: input integer
  * Return: sum, n==0, return 0
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned in sum = 0, i;
+	unsigned int sum = 0, i;
 	va_list ptr;
 
 	if (n == 0)
@@ -18,8 +18,8 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
-	
+
 	va_end(ptr);
-	
+
 	return (sum);
 }
