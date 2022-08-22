8754945d3609a5abb82719100b07cd43d5570e28
AngieKoki <angie.dar66@gmail.com>
2022-08-22 10:05:02 -0700

    Copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -10,5 +11,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void _error(int e, char *filename);
+void cp(char *file_from, char *file_to);
 
 #endif
